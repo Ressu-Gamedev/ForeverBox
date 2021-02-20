91c50f3c3a141916ac7fd241ff6b02099782b48f
PixelSergey <sergey.ichtchenko@gmail.com>
2021-02-20 23:51:56 +0200

    Initialise new game with Unity
    Featuring fully-functional dialogue system; quite insane

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> dialogue;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public Animator animator;
+
+    void Start()
+    {
+        dialogue = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue toPlay){
+        //Debug.Log("Starting " + toPlay.name);
+        animator.SetBool("IsOpen", true);
+        nameText.text = toPlay.name;
+        dialogue.Clear();
+        foreach (string phrase in toPlay.dialogue){
+            dialogue.Enqueue(phrase);
+        }
+
+        DisplayNextPhrase();
+    }
+
+    public void DisplayNextPhrase(){
+        if(dialogue.Count == 0){
+            EndDialogue();
+            return;
+        }
+        string phrase = dialogue.Dequeue();
+        //Debug.Log(phrase);
+        dialogueText.text = phrase;
+    }
+
+    void EndDialogue(){
+        //Debug.Log("Ending dialogue");
+        animator.SetBool("IsOpen", false);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Infinity Box
+
+February 2021 game jam: Team A
