c8350f2a2daee58c5f08928dc380b621db07b418
PixelSergey <sergey.ichtchenko@gmail.com>
2021-02-22 01:57:05 +0200

    Compelete game
    YAAAyayyayayayayayayyayaya

---
+++ Assets/Scripts/Boss3.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Boss3 : Boss
+{
+    public Sprite[] images;
+    public string[] text;
+    private int location = 0;
+    public TextMeshProUGUI prompt;
+    public float timer = 0f;
+    public int reps = 0;
+    public SpriteRenderer body;
+    public GameObject cam1;
+    public GameObject cam2;
+    public AudioSource audioS;
+    public AudioClip bzzz;
+    public AudioClip bzzz2;
+    public GameObject finale;
+    public Vector2 newPos;
+    public GameObject player;
+
+
+    void Start()
+    {
+        finale.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    void Update(){
+        if(!isActive) return;
+        prompt.gameObject.SetActive(true);
+        //Debug.Log("fsfsf");
+        timer += Time.deltaTime;
+        if(timer >= 1.5f){
+            timer = 0;
+            reps++;
+            int index = Random.Range(0, 3);
+            body.sprite = images[index];
+        }
+
+        if(location >= text.Length){
+            finale.SetActive(true);
+            audioS.clip = bzzz;
+            audioS.volume = 0.4f;
+            audioS.Play();
+            audioS.PlayOneShot(bzzz2);
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+            prompt.gameObject.SetActive(false);
+            player.transform.position = newPos;
+            Destroy(this);
+            return;
+        }
+
+        prompt.text = "TYPE: " + text[location].ToUpper();
+        if(Input.GetKeyDown(text[location])){
+            location++;
+        }
+
+    }
+}
--- Assets/Scripts/TalkTrigger.cs
+++ Assets/Scripts/TalkTrigger.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class TalkTrigger : DialogueTrigger
 {
     public AudioSource audioS;
     public AudioClip sfx;
+    public bool end = false;
+    public bool trig = false;
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player"))
         {
             TriggerDialogue();
             audioS.PlayOneShot(sfx);
+            if(!end) Destroy(this);
+            trig = true;
+        }
+    }
+
+    void Update(){
+        if(!end) return;
+        if(!manager.inProgress && trig){
+            PlayerPrefs.SetInt("playthrough", 1);
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
             Destroy(this);
         }
     }
